17e4562fa37eedbef2865493d53856744668a134
intersoom <sallybig@naver.com>
2021-07-30 03:12:00 +0900

    #1 둘러보기 기능

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,2 +1,2 @@
 export {default as Menu} from './Menu';
-export {default as Navbar} from './Navbar';
+export {default as Navbar} from './navbar';
--- client/src/routes/Main.js
+++ client/src/routes/Main.js
@@ -3,37 +3,54 @@ import {Menu, Navbar} from '../components';
 import {Comment, Tooltip, Avatar} from 'antd';
 import moment from 'moment';
 import '../style/Main.css';
-const Main = () => {
+import logo from '../assets/images/logo.png';
+
+class Main extends React.Component {
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      
+    }
+  }
+
+  render(){
   return (
-    <div>
-      <h1>메인화면</h1>
-      <p>이 프로젝트는 리액트 라우터 기초를 실습해보는 예제 프로젝트랍니다.</p>
-      <div className="comment">
-        <Comment
-          author={<a>Han Solo</a>}
-          avatar={
-            <Avatar
-              src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
-              alt="Han Solo"
-            />
-          }
-          content={
-            <p>
-              We supply a series of design principles, practical patterns and high quality design
-              resources (Sketch and Axure), to help people create their product prototypes beautifully
-              and efficiently.
-            </p>
-          }
-          datetime={
-            <Tooltip title={moment().format('YYYY-MM-DD HH:mm:ss')}>
-              <span>{moment().fromNow()}</span>
-            </Tooltip>
-          }
-        />
-      </div>
-      <Navbar />
+    <div class="wrap">
+        <div id="test"></div>
+      {/* 내용이 있는 부분*/}
+        <div class="inner-box">
+          <div class="backgroundShape">
+            {/*배경 이미지 위에 올라가는 헤더 부분*/}
+          <div class="header">
+            <h1>모아보기</h1>
+            <div class="logo">
+              <img src={logo} alt="logo"></img>
+            </div>
+            <h4>카테고리를 선택해주세요</h4>
+            {/* 클릭되면 폰트 색 바뀌게  */}
+            <input type="button" value="지역별" class="categoryButton region"></input>
+            <input type="button" value="대학별" class="categoryButton university"></input>
+            <input type="button" value="관심사별" class="categoryButton interest"></input>
+          </div>
+          </div>
+          
+          {/*파드*/}
+          <div class="feedWrap">
+            <h2>최신 피드</h2> {/*카테고리마다 바뀜, js로 받아서 수정해야함*/}
+            <h3>384건의 새로운 피드</h3> {/*숫자는 js로 받아서 수정*/}
+            <div class="feedImage">
+              {/* <div class="feedText">같이 게임 할 사람</div> 이거는 div로 할지 텍스트로 할지 추후 해보고 수정! (아마 텍스트로 해야할듯?) */}
+            </div>
+          </div>
     </div>
-  );
-};
+
+      {/*bottom navigation*/}
+      <Navbar />
+        </div>
+  
+        );
+      }
+    };
 
 export default Main;
