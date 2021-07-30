2e85e709ec92f6f2a354ea1fbd257f5e5576b092
geun9716 <crintop@gmail.com>
2021-07-31 00:58:28 +0900

    #5 profile update 처리중

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -2,11 +2,25 @@ var express = require('express');
 import profile from '../controller/profile'
 const router = express.Router();
 
+var multer = require('multer') // 
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/images');
+      },
+      filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname.replaceAll(' ', '_'));
+      }
+})
+// var upload = multer({ dest: 'uploadedFiles/' }); // 3-1
+var upload = multer({ storage: storage }); // 3-2
+
 // 프로필 일반
 router.get('/:uid', profile.normal);
 
 // 프로필 상세
 router.get('/detail/:uid', profile.detail);
+
+router.put('/:uid', upload.single('images'), profile.update);
 
 
 export default router
