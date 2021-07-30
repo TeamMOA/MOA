3c11585040e64c6babeb3ebe2efea7e312954b15
geun9716 <crintop@gmail.com>
2021-07-30 20:40:44 +0900

    Navigationbar fix2

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,3 @@
 export {default as Menu} from './Menu';
-export {default as Navbar} from './Navbar';
+export {default as Navbar} from './Navigationbar';
 export {default as Category} from './Category';
