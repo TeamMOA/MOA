bccb9cd3f5d4ad1a90db50ed654fb00e868182c2
intersoom <sallybig@naver.com>
2021-07-30 21:50:55 +0900

    #01 버튼 수정 완료

--- client/src/routes/Main.js
+++ client/src/routes/Main.js
@@ -14,7 +14,110 @@ import { Menu, ActivityIndicator, NavBar } from 'antd-mobile';
 // const { Header, Content, Footer, Sider } = Layout;
 // const { SubMenu } = Menu;
 
+const regionData = [
+  {
+    value: '1',
+    label: '경기',
+  }, {
+    value: '2',
+    label: '서울',
+  },
+  {
+    value: '3',
+    label: '충청',
+    isLeaf: true,
+  },
+  {
+    value: '4',
+    label: '강원',
+    isLeaf: true,
+  },
+  {
+    value: '5',
+    label: '전라',
+    isLeaf: true,
+  },
+  {
+    value: '5',
+    label: '경상',
+    isLeaf: true,
+  },
+  {
+    value: '5',
+    label: '제주',
+    isLeaf: true,
+  },
+];
 
+const univData = [
+  {
+    value: '1',
+    label: '서울대',
+  }, {
+    value: '2',
+    label: '숭실대',
+  },
+  {
+    value: '3',
+    label: '고려대',
+    isLeaf: true,
+  },
+  {
+    value: '4',
+    label: '홍익대',
+    isLeaf: true,
+  },
+  {
+    value: '5',
+    label: '해양대',
+    isLeaf: true,
+  },
+  {
+    value: '6',
+    label: '서강대',
+    isLeaf: true,
+  },
+  {
+    value: '7',
+    label: '한국대',
+    isLeaf: true,
+  },
+];
+
+const interestData = [
+  {
+    value: '1',
+    label: '미술',
+  }, {
+    value: '2',
+    label: '게임',
+  },
+  {
+    value: '3',
+    label: '운동',
+    isLeaf: true,
+  },
+  {
+    value: '4',
+    label: '음악',
+    isLeaf: true,
+  },
+  {
+    value: '5',
+    label: '취업',
+    isLeaf: true,
+  },
+  {
+    value: '6',
+    label: '공부',
+    isLeaf: true,
+  },
+  {
+    value: '7',
+    label: '뜨개질',
+    isLeaf: true,
+  },
+];
 
 class Main extends React.Component {
   
@@ -53,9 +156,9 @@ class Main extends React.Component {
                   this.state.region = ['서울', '부산', '대전'];
                 }}></input>
                 <input type="button" value="관심사별" className="categoryButton interest" onClick={() => {alert('지역별')}}></input> */}
-              <Button categoryName="지역별"/>
-              <Button categoryName="학교별"/>
-              <Button categoryName="관심사별"/>
+              <Button categoryName="지역별" data={regionData}/>
+              <Button categoryName="학교별" data={univData}/>
+              <Button categoryName="관심사별" data={interestData}/>
             </div>
             <div className="feedWrap">
               <div className="feedTitle">
