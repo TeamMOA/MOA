93d0a7791dac92a3b1b67640ffd8205b321ba0bd
geun9716 <crintop@gmail.com>
2021-07-31 00:01:40 +0900

    #4 로그인 결국 진짜기능...

--- server/controller/user.js
+++ server/controller/user.js
@@ -4,26 +4,26 @@ import security from '../middleware/security'
 
 // 로그인
 async function login(req, res) {
+    console.log(req.body);
     try {
-        const userEmail = req.body.userEmail
+        const userEmail = req.body.email
         const userPw = req.body.userPw
-
-        const selectKey = await db.query("select userKey from users where email = ?", [userEmail])
-
+        
+        const selectKey = await db.query("select userKey from users where email = ?", [userEmail]);
         const secret = await security.pbkdf2(userPw, selectKey[0].userKey)
 
         const pw = secret.pw
 
-        let userInfo = await db.query('select userID, email, nickname, introduce from users where email = ? AND password = ?', [userEmail, pw])
+        let userInfo = await db.query('select userID, email, nickname from users where email = ? AND password = ?', [userEmail, pw])
 
         if(userInfo.length > 0){
             userInfo = userInfo[0]
             const returnObj = {
                 success : true,
+                msg:'로그인에 성공해따...',
                 userId : userInfo.userID,
                 userEmail : userInfo.email,
                 nickname : userInfo.nickname,
-                introduce : userInfo.introduce,
             }
             res.status(httpStatus.OK).send(returnObj)
         } else{
@@ -41,40 +41,72 @@ async function login(req, res) {
 // 회원가입
 async function signUp(req, res) {
     try {
-        console.log(req.body);
         const userEmail = req.body.email;
-        const userPw = req.body.userPw;
-        const nickname = req.body.nickname;
-        const introduce = req.body.introduce;
-        const secret = await security.security(userPw);
         
-        let signUp = await db.query('insert into users set ?', {
-            email : userEmail,
-            password : secret.pw,
-            userKey : secret.pwKey,
-            nickname : nickname,
-            introduce : introduce
-        })
-        if(signUp.errno > 0) {
+        const isValid = await db.query('select uid from users where email = ?', [userEmail]);
+        console.log(isValid);
+        if (isValid.length > 0){
             res.status(httpStatus.NOT_FOUND).send({
                 success : false,
-                message : "Failed to sign up"
+                message : "Existed Email"
             })
-        }
-        else {
-            res.status(httpStatus.OK).send({
-                success : true,
-                message : "Register user information successfully"
-            })
-        }
+        } else {
+            const userPw = req.body.userPw;
+            const userID = req.body.userID;
+            const nickname = req.body.nickname;
+            const secret = await security.security(userPw);
 
+            let signUp = await db.query('insert into users set ?', {
+                email : userEmail,
+                userID : userID,
+                password : secret.pw,
+                userKey : secret.pwKey,
+                nickname : nickname,
+            })
+            if(signUp.errno > 0) {
+                res.status(httpStatus.NOT_FOUND).send({
+                    success : false,
+                    message : "Failed to sign up"
+                })
+            }
+            else {
+                res.status(httpStatus.OK).send({
+                    success : true,
+                    message : "Register user information successfully"
+                })
+            }
+        }
     } catch (error) {
         console.error(error, "signUp api error")
         res.status(httpStatus.INTERNAL_SERVER_ERROR).send([])
     }
 }
 
+async function checkDuplicate(req, res) {
+    try {
+        const userID = req.body.userID
+        
+        let isDuplicated = await db.query('select uid from users where userID = ?', [userID]);
+
+        if(isDuplicated.length > 0){
+            res.status(httpStatus.NOT_FOUND).send({
+                success : false,
+                msg : 'Exited userID'
+            });
+        } else{
+            res.status(httpStatus.NOT_FOUND).send({
+                success : true,
+                message : "아이디를 사용하실 수 있습니다."
+            })
+        }
+   } catch(error) {
+        console.error(error, "checkDupilcate api error")
+        res.status(httpStatus.INTERNAL_SERVER_ERROR).send([])
+   }
+}
+
 export default {
     login,
     signUp,
+    checkDuplicate,
 }
