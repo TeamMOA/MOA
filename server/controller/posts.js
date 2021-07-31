70cf470fb04f9aee035d45f985ecfc98688db99d
geun9716 <crintop@gmail.com>
2021-07-31 17:03:39 +0900

    #6 post API 추가

--- server/controller/posts.js
+++ server/controller/posts.js
@@ -79,6 +79,7 @@ async function getPostId (req, res) {
 }
 
 async function createPost (req, res) {
+    const pageSize = req.query.pageSize?parseInt(req.query.pageSize):20;
     try {
         const uid = req.body.uid;
         const nickname = req.body.nickname;
@@ -147,9 +148,61 @@ async function deletePost (req, res) {
    }
 }
 
+async function getPostFilter (req, res) {
+    const pageSize = req.query.pageSize?parseInt(req.query.pageSize):20;
+    const region = req.body.region;
+    const univ = req.body.univ;
+    const interest = req.body.interest;
+    let msg = [];
+    if (region){
+        msg.push(`p.region REGEXP '${region}'`);
+    }
+    if (univ) {
+        msg.push(`p.univ REGEXP '${univ}'`);
+    }
+    if (interest) {
+        msg.push(`p.interest REGEXP '${interest}'`);
+    }
+    let wheremsg = `WHERE ${msg.join(" AND ")}`;
+    console.log(wheremsg);
+    try {
+        let result = await db.query(`SELECT pid, content, userCount, posttime, p.region, p.univ, p.interest, img, u.uid, u.profileImg FROM posts p JOIN users u ON p.uid=u.uid ${region||univ||interest?wheremsg:null} ORDER BY posttime DESC LIMIT ?;`, [pageSize]);
+        let posts = [];
+        if(result.length > 0){
+            result.map((val) => {
+                posts.push({
+                    pid : val.pid,
+                    uid : val.uid,
+                    nickname : val.nickname,
+                    content : val.content,
+                    userCount: val.userCount,
+                    posttime: modules.timestamp(val.posttime),
+                    region : val.region,
+                    univ : val.univ,
+                    interest : val.interest,
+                    img: val.img,
+                    link : val.link,
+                    profileImg : val.profileImg,
+                });
+            });
+            const returnObj = {
+                success : true,
+                posts : posts
+            }
+            res.status(httpStatus.OK).send(returnObj)
+        } else{
+            res.status(httpStatus.NOT_FOUND).send()
+        }
+   } catch(error) {
+        console.error(error, "posts api error")
+        res.status(httpStatus.INTERNAL_SERVER_ERROR).send([])
+   }
+}
+
 export default {
     getPosts,
     getPostId,
     createPost,
+    getPostFilter,
     deletePost,
 }
